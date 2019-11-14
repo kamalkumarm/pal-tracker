6672bcb12ba6f0d06f9223125eeafa8ffdc6dc54
kamalkumar <kamalkumar.madhavan@gmail.com>
2019-11-14 11:30:23 +0530

    Recall why ORMs such as JPA encourage circular dependencies Describe how to set up a Spring app to connect to a MySQL database locally Describe how to set up a Spring app running on CF to connect to a MySQL CF Service Use JDBC Template to perform CRUD database operations

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,5 +1,9 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +19,23 @@ public class TimeEntryController {
 
 
     private TimeEntryRepository timeEntryRepository;
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryController(TimeEntryRepository timeEntryRepository,
+                               MeterRegistry meterRegistry) {
         this.timeEntryRepository = timeEntryRepository;
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
 
     @GetMapping("/{ID}")
     public ResponseEntity<TimeEntry> read(@PathVariable("ID") long timeEntryId) {
         TimeEntry localtimeEntry = timeEntryRepository.find(timeEntryId);
-        if(localtimeEntry != null)
+        if(localtimeEntry != null) {
+            actionCounter.increment();
             return new ResponseEntity<>(localtimeEntry, HttpStatus.OK);
+        }
         else
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
@@ -33,8 +44,10 @@ public class TimeEntryController {
     public ResponseEntity update(@PathVariable("ID") long timeEntryId, @RequestBody TimeEntry expected) {
         TimeEntry localtimeEntry = timeEntryRepository.update(timeEntryId, expected);
 
-        if(localtimeEntry != null)
+        if(localtimeEntry != null) {
+            actionCounter.increment();
             return new ResponseEntity<>(localtimeEntry, HttpStatus.OK);
+        }
         else
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
@@ -42,17 +55,23 @@ public class TimeEntryController {
     @DeleteMapping("/{ID}")
     public ResponseEntity delete(@PathVariable("ID") long timeEntryId) {
         timeEntryRepository.delete(timeEntryId);
+        actionCounter.increment();
+        timeEntrySummary.record(timeEntryRepository.list().size());
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("")
     public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
-        return new ResponseEntity<>(timeEntryRepository.create(timeEntryToCreate), HttpStatus.CREATED);
+        TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
+        actionCounter.increment();
+        timeEntrySummary.record(timeEntryRepository.list().size());
+        return new ResponseEntity<>(timeEntry, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
         List<TimeEntry> listEntries = new ArrayList<>(timeEntryRepository.list());
+        actionCounter.increment();
         return new ResponseEntity<>(listEntries, HttpStatus.OK);
     }
 }
